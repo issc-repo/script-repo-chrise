9d64db80b766176358fc3a66cb28841b0d2f53ec
chris-sorandom <161941128+chris-sorandom@users.noreply.github.com>
2024-03-29 10:28:57 +0200

    Update Winget - Run as System.ps1

--- General/Windows Update/Winget - Run as System.ps1
+++ General/Windows Update/Winget - Run as System.ps1
@@ -1,3 +1,7 @@
+#Install VC Dependancies if not already installed
+wget "https://aka.ms/vs/17/release/vc_redist.x64.exe" -Outfile C:\vcredist_x64.exe
+Start-Process -FilePath “C:\vcredist_x64.exe” -ArgumentList “/Q /norestart” -Wait
+Remove-Item C:\vcredist_x64.exe -Force
 # Find Winget Directory
 $wgdir=(Get-Childitem -Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller*" -Include winget.exe -Recurse -ErrorAction SilentlyContinue | Select-Object -Last 1 | %{$_.FullName} | Split-Path)
 # Move to WinGet Directory
