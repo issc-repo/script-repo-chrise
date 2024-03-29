2d48a2730731ff2860bc72cf1d36ce4052d5bf9e
chris-sorandom <161941128+chris-sorandom@users.noreply.github.com>
2024-03-29 10:23:05 +0200

    Create Winget - Run as System.ps1

---
+++ General/Windows Update/Winget - Run as System.ps1
@@ -0,0 +1,6 @@
+# Find Winget Directory
+$wgdir=(Get-Childitem -Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller*" -Include winget.exe -Recurse -ErrorAction SilentlyContinue | Select-Object -Last 1 | %{$_.FullName} | Split-Path)
+# Move to WinGet Directory
+cd $wgdir
+#Run Winget Upgrade All
+.\winget.exe upgrade --all --accept-package-agreements --accept-source-agreements --scope=machinee
