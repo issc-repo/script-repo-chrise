971d52149d6ee8016d011d245a88402805569fc4
chris-sorandom <161941128+chris-sorandom@users.noreply.github.com>
2024-06-26 12:33:08 +0200

    Update Remove Delegated Access from All Mailboxes for single user.ps1

--- General/Exchange Online/Remove Delegated Access from All Mailboxes for single user.ps1
+++ General/Exchange Online/Remove Delegated Access from All Mailboxes for single user.ps1
@@ -4,3 +4,5 @@ $mailboxes=Get-Mailbox
 $mailboxes | foreach {Remove-MailboxPermission -Identity  $_.UserPrincipalName -user $exuser -AccessRights FullAccess -Confirm:$False -BypassMasterAccountSid}
 $mailboxes | foreach {Remove-MailboxPermission -Identity  $_.UserPrincipalName -user $exuser -AccessRights SendAs -Confirm:$False -BypassMasterAccountSid}
 $mailboxes | foreach {Remove-RecipientPermission -Identity  $_.UserPrincipalName -AccessRights SendAs -Trustee $exuser -Confirm:$False}
+$mailboxes | foreach {Remove-RecipientPermission -Identity  $_.UserPrincipalName -AccessRights FullAccess -Trustee $exuser -Confirm:$False}
+$mailboxes | ForEach {Remove-MailboxFolderPermission -Identity "$($_.PrimarySMTPAddress):\Calendar" -User $exuser -Confirm:$False}
