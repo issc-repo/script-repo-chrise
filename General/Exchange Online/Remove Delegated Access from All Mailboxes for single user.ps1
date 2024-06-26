77359c1c22af2dc0d3ce73bdc8cf8347cae2cb9f
chris-sorandom <161941128+chris-sorandom@users.noreply.github.com>
2024-06-26 12:19:43 +0200

    Update Remove Delegated Access from All Mailboxes for single user.ps1

--- General/Exchange Online/Remove Delegated Access from All Mailboxes for single user.ps1
+++ General/Exchange Online/Remove Delegated Access from All Mailboxes for single user.ps1
@@ -1,4 +1,6 @@
 Connect-ExchangeOnline
+$exuser='christopher@cloudboxtech.com'
 $mailboxes=Get-Mailbox 
-$mailboxes | foreach {Remove-MailboxPermission -Identity  $_.UserPrincipalName -user christopher@cloudboxtech.com -AccessRights FullAccess -Confirm:$False -BypassMasterAccountSid}
-$mailboxes | foreach {Remove-MailboxPermission -Identity  $_.UserPrincipalName -user christopher@cloudboxtech.com -AccessRights SendAs -Confirm:$False -BypassMasterAccountSid}
+$mailboxes | foreach {Remove-MailboxPermission -Identity  $_.UserPrincipalName -user $exuser -AccessRights FullAccess -Confirm:$False -BypassMasterAccountSid}
+$mailboxes | foreach {Remove-MailboxPermission -Identity  $_.UserPrincipalName -user $exuser -AccessRights SendAs -Confirm:$False -BypassMasterAccountSid}
+$mailboxes | foreach {Remove-RecipientPermission -Identity  $_.UserPrincipalName -AccessRights SendAs -Trustee $exuser -Confirm:$False}
