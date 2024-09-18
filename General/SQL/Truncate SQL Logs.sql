72182413f5964223237fde5c16df11cd3841d683
chris-sorandom <161941128+chris-sorandom@users.noreply.github.com>
2024-09-18 10:26:58 +0200

    Update Truncate SQL Logs.sql

--- General/SQL/Truncate SQL Logs.sql
+++ General/SQL/Truncate SQL Logs.sql
@@ -1,13 +1,13 @@
-USE GuESS;  
+USE DB_Name;  
 GO  
 -- Truncate the log by changing the database recovery model to SIMPLE.  
-ALTER DATABASE GuESS
+ALTER DATABASE DB_Name
 SET RECOVERY SIMPLE;  
 GO  
 -- Shrink the truncated log file to 1 MB.  
-DBCC SHRINKFILE (GuESS_log, 1);  
+DBCC SHRINKFILE (DB_log, 1);  
 GO  
 -- Reset the database recovery model.  
-ALTER DATABASE GuESS
+ALTER DATABASE DB_Name
 SET RECOVERY FULL;  
 GO 
