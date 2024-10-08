aa2e19db3c6862d037950107b726e13cf01b4251
chris-sorandom <161941128+chris-sorandom@users.noreply.github.com>
2024-10-08 15:12:54 +0200

    Update Linux-AzureConsole-InstallRMM.sh

--- General/Datto RMM/Linux-AzureConsole-InstallRMM.sh
+++ General/Datto RMM/Linux-AzureConsole-InstallRMM.sh
@@ -1,2 +1,2 @@
-sudo wget https://merlot.centrastage.net/csm/profile/downloadLinuxAgent/20cffc6f-5a68-4f5b-8d75-4020e64b13ec -O rmm.sh
+sudo wget https://merlot.centrastage.net/csm/profile/downloadLinuxAgent/$site-id -O rmm.sh
 sudo sh rmm.sh
