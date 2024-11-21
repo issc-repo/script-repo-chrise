54a2ce23893fbf9a05cc504cdf7d0405854ca296
Christopher Evans <chris@sorandom.co.za>
2024-11-21 11:59:28 +0200

    Previous commit did not correctly apply

---
+++ General/Cloudflare/Worker - sorandom-co-za-mta-sts.js
@@ -0,0 +1,17 @@
+async function handleRequest(request) {
+  const init = {
+    headers: {
+      'content-type': 'text/plain; charset=UTF-8',
+      'X-Clacks-Overhead': 'GNU Terry Pratchett',
+    },
+  }
+  return new Response(stsHTML, init)
+}
+addEventListener('fetch', event => {
+  return event.respondWith(handleRequest(event.request))
+})
+const stsHTML = `version: STSv1
+mode: enforce
+mx: mail.sorandom.co.za
+max_age: 86400
+`
