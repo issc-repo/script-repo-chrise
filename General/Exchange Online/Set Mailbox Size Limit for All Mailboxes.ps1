2ddb452a8fbb8ad6baee5a224a9b8e6f52ccf3e4
Christopher Evans <chris@sorandom.co.za>
2024-12-02 12:24:03 +0200

    New Script - Set EXO send and Receive limits for all users

--- General/Exchange Online/Set Mailbox Size Limit for All Mailboxes.ps1
+++ General/Exchange Online/Set Mailbox Size Limit for All Mailboxes.ps1
@@ -1,9 +1,7 @@
-# This does not increase the organisation default limit, this will need to be done via the Exchange Online GUI
-
 # Connect to Exchange Online
 Connect-ExchangeOnline
 
-# Set send/receive limits for all users to 150 MB
+# Set send / receive limits for all users to 150 MB
 Get-Mailbox | Set-Mailbox -MaxSendSize 150mb -MaxReceiveSize 150mb
 
 # Confirm it applied
