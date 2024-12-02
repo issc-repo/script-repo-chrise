1f6302371b385f2474eb26146e8320af9d7f8120
Christopher Evans <chris@sorandom.co.za>
2024-12-02 12:44:49 +0200

    Fix rebase with mailbox limit script

--- General/Exchange Online/Set Mailbox Size Limit for All Mailboxes.ps1
+++ General/Exchange Online/Set Mailbox Size Limit for All Mailboxes.ps1
@@ -5,6 +5,8 @@ Connect-ExchangeOnline
 
 # Set send / receive limits for all users to 150 MB
 Get-Mailbox | Set-Mailbox -MaxSendSize 150mb -MaxReceiveSize 150mb
+Get-MailboxPlan | Set-MailboxPlan -MaxSendSize 150mb -MaxReceiveSize 150mb
 
 # Confirm it applied
 Get-Mailbox | ft name, maxsendsize, maxreceivesize
+Get-MailboxPlan | fl name, MaxSendSize, MaxReceiveSize
