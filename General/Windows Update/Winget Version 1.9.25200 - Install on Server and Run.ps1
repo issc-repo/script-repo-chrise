3e18079db556428828c7105cc8a4ed2581ab2260
chris-sorandom <161941128+chris-sorandom@users.noreply.github.com>
2024-12-02 16:11:25 +0200

    Update Winget Version 1.9.25200 - Install on Server and Run.ps1

--- General/Windows Update/Winget Version 1.9.25200 - Install on Server and Run.ps1
+++ General/Windows Update/Winget Version 1.9.25200 - Install on Server and Run.ps1
@@ -27,13 +27,13 @@ foreach ($folder in $folders) {
     ICACLS $folderPath /grant Administrators:F /T
 }
 
-$ResolveWingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe"
-    if ($ResolveWingetPath){
-           $WingetPath = $ResolveWingetPath[-1].Path
-    }
-$ENV:PATH += ";$WingetPath"
-
-winget upgrade --all --accept-package-agreements --accept-source-agreements
+# Find Winget Directory
+$wgdir=(Get-Childitem -Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller*" -Include winget.exe -Recurse -ErrorAction SilentlyContinue | Select-Object -Last 1 | %{$_.FullName} | Split-Path)
+# Move to WinGet Directory
+cd $wgdir
+#Run Winget Upgrade All (first upgrade machine-scoped apps, then user scoped apps)
+.\winget.exe upgrade --all --accept-package-agreements --accept-source-agreements --scope=machine
+.\winget.exe upgrade --all --accept-package-agreements --accept-source-agreements
 
 cd C:\
 
