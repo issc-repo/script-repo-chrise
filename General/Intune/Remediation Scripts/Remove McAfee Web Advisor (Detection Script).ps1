4f37a7235985a2238aea6503fee78ccc9b427be1
Christopher Evans <chris@sorandom.co.za>
2025-01-08 12:27:14 +0200

    Updated Remediation scripts for McAfee Web Advisor removal

--- General/Intune/Remediation Scripts/Remove McAfee Web Advisor (Detection Script).ps1
+++ General/Intune/Remediation Scripts/Remove McAfee Web Advisor (Detection Script).ps1
@@ -7,9 +7,3 @@ if (Test-Path 'C:\Program Files\McAfee\WebAdvisor') {
     Write-Host McAffee Web Advisor Not Installed
     Exit 0
   }
-
-#Remove McAfee Web Advisor (Remediation Script)
-cd "C:\Program Files\McAfee\WebAdvisor"
-.\uninstaller.exe /s
-cd "C:\Program Files"
-Remove-Item McAfee -Force
