e9a9bc2a338958a75ab484f00e1b59b2e1f6fe30
Christopher Evans <chris@sorandom.co.za>
2025-01-09 11:11:19 +0200

    Move Cloudbox-specific data to it's own Repo

--- Company-Specific/CBX/GlobalRelayMeteredAPI.ps1
+++
@@ -1,6 +0,0 @@
-# Steel Eye Metered API Enablement
-# Enable Microsoft.GraphServices Resource Provider on Subscription
-# Run in Cloud PowerShell Instance
-# Swap out AppID and SubScription ID as required, target resource group must exist before running this.
-
-az resource create --resource-group GlobalRelay --name GlobalRelayAppBilling --resource-type Microsoft.GraphServices/accounts --properties  "{`"appId`": `"f7f246a4-1fc4-4bc4-a865-624ce7d29428`"}" --location Global --subscription bbfd0737-2b60-43f6-a2a5-331dd8df64c4
--- Company-Specific/Genasys/Enable-WSMan.ps1
+++
@@ -1,9 +0,0 @@
-If (!(Test-Path C:\WSMANCresSSPEnabbled.txt)) {
-    Enable-WSManCredSSP -Role Client -DelegateComputer '*.genasys.tech' -Force
-    New-Item C:\WSMANCresSSPEnabbled.txt -Force
-    Set-Content C:\WSMANCresSSPEnabbled.txt 'WSMan has been enabled with CredSSP Support' -Force
-}
-else {
-    Write-Output 'WinRM Delegation Already Enabled'
-    Exit 0
-}
