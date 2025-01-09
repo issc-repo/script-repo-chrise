ad105c7e2a07d9eb39a60a2bd9ffb896c25e9b15
Christopher Evans <chris@sorandom.co.za>
2025-01-09 11:02:11 +0200

    Remove iSSC Specific files from general repo

--- Company-Specific/iSSC/Intune Policy Templates/RSA - 2025-01-09/DeviceHealthScripts/Remove McAfee Web Advisor_DetectionScript.ps1
+++
@@ -1,9 +0,0 @@
-#Scan for McAfee Web Advisor (Detection Script)
-if (Test-Path 'C:\Program Files\McAfee\WebAdvisor') {
-    Write-Host McAffee Web Advisor Installed
-    Exit 1
-  }
-  Else {
-    Write-Host McAffee Web Advisor Not Installed
-    Exit 0
-  }
--- Company-Specific/iSSC/Intune Policy Templates/RSA - 2025-01-09/PowerShellScripts/Install Adobe Reader via Winget (Run as System).ps1
+++
@@ -1,5 +0,0 @@
-# Find Winget Directory
-$wgdir=(Get-Childitem -Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller*" -Include winget.exe -Recurse -ErrorAction SilentlyContinue | Select-Object -Last 1 | %{$_.FullName} | Split-Path)
-# Move to WinGet Directory
-cd $wgdir
-.\winget.exe install --id=Adobe.Acrobat.Reader.64-bit -e --accept-package-agreements --accept-source-agreements
