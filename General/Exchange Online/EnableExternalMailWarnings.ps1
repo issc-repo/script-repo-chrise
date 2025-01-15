a247d8acd1e1faea1fb54d06a9db3665e34c645d
Christopher Evans <chris@sorandom.co.za>
2025-01-15 11:07:13 +0200

    Readability update to External warnings script for EXO

--- General/Exchange Online/EnableExternalMailWarnings.ps1
+++ General/Exchange Online/EnableExternalMailWarnings.ps1
@@ -1,8 +1,11 @@
 Connect-ExchangeOnline
 
+#Enable External Tags in Outlook
 Set-ExternalInOutlook –Enabled $true
 
+#Set up external sender Disclaimer (Prepend to emails)
 $Disclaimer='<p><div style="background-color:#FFEB9C; width:100%; border-style: solid; border-color:#9C6500; border-width:1pt; padding:2pt; font-size:10pt; line-height:12pt; font-family:Calibri; color:Black; text-align: left;"><span style="color:#9C6500"; font-weight:bold;>CAUTION:</span> This email originated from outside of the organization. Do not click links or open attachments unless you recognize the sender and know the content is safe.</div><br></p>'
 New-TransportRule "External Email Warning" -FromScope NotInOrganization -SentToScope InOrganization -Priority 0 -ApplyHtmlDisclaimerText $Disclaimer -ApplyHtmlDisclaimerLocation Prepend -ApplyHtmlDisclaimerFallbackAction Wrap
 
+#Enable External Recipient Warning Mailtip
 Set-OrganizationConfig -MailTipsExternalRecipientsTipsEnabled:$True
