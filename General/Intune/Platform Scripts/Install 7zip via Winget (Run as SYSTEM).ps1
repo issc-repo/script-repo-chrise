9a1042f5c003fa0f173002b33fe3de0e851c8bac
Christopher Evans <chris@sorandom.co.za>
2025-01-15 08:48:51 +0200

    Created Intune Platform scripts to install 7zip and Anydesk

---
+++ General/Intune/Platform Scripts/Install 7zip via Winget (Run as SYSTEM).ps1
@@ -0,0 +1,5 @@
+# Find Winget Directory
+$wgdir=(Get-Childitem -Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller*" -Include winget.exe -Recurse -ErrorAction SilentlyContinue | Select-Object -Last 1 | %{$_.FullName} | Split-Path)
+# Move to WinGet Directory
+cd $wgdir
+.\winget.exe install --id=7zip.7zip -e --accept-package-agreements --accept-source-agreements
---
+++ General/Intune/Platform Scripts/Install AnyDesk via Winget (Run as SYSTEM).ps1
@@ -0,0 +1,5 @@
+# Find Winget Directory
+$wgdir=(Get-Childitem -Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller*" -Include winget.exe -Recurse -ErrorAction SilentlyContinue | Select-Object -Last 1 | %{$_.FullName} | Split-Path)
+# Move to WinGet Directory
+cd $wgdir
+.\winget.exe install --id=AnyDeskSoftwareGmbH.AnyDesk -e --accept-package-agreements --accept-source-agreements
